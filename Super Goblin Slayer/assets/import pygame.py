9077acac5fc173dbd97631b4ab97aacf0eb8e7d7
Lakezin Jamtsho <141400778+Laketsho187@users.noreply.github.com>
2023-11-02 19:42:37 +0600

    Add files via upload

---
+++ Super Goblin Slayer/assets/import pygame.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+
+pygame.init()
+
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
+WHITE = (255, 255, 255)
+RED = (255, 0 , 0)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.dispaly.set_caption("type shot")
